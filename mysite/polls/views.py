6c43b3dd47f0e03f4875ac09a550ec2b909c77d1
Le Xiao <xiaole@atrihub.io>
2017-07-26 10:11:22 -0700

    feat(LX Django Training): Chapter 5
    Created tests for exposing bugs and views
    
    ICTRAINING-17

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 #    from django.http import Http404
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -29,7 +30,7 @@ def detail(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question':question})
+    return render(request, 'polls/results.html', {'question': question})
     return HttpResponse(response % question_id)
 
 
@@ -38,12 +39,15 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
